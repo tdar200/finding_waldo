660a6f54ecaa03e80776e1caf2741e6d6c013104
Tahseen <tahseendar01@gmail.com>
2021-12-25 19:58:59 +0500

    heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -29,13 +29,12 @@ const connectDB = async () => {
     } catch (error) {
       console.error(error);
     }
+  });
+  const publicPath = path.join(__dirname, "../frontend/build");
+  app.use(express.static(publicPath));
 
-    const publicPath = path.join(__dirname, "../frontend/build");
-    app.use(express.static(publicPath));
-
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(publicPath, "index.html"));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(publicPath, "index.html"));
   });
 };
 
